cc5287e41b8812aa6cd7c0e68bc0a82735dc55db
CraniumAdamantium <af.carrillo@live.com>
2021-08-30 19:22:03 -0400

    Versión inicial de spring con mongodb y lombok

---
+++ src/main/java/malazos/S4Java/S4JavaApplication.java
@@ -0,0 +1,13 @@
+package malazos.S4Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S4JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S4JavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/malazos/S4Java/S4JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package malazos.S4Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S4JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
